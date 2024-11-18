fd64202d1eb7ca5815a2c0b8a9e81bdb615c20c9
Kishore-Vijayakumar <kishorev.primary@gmail.com>
2024-11-18 21:02:19 +0530

    First commit

---
+++ sample_python.py
@@ -0,0 +1,10 @@
+# This is a sample python file to learn working of git
+# This program just prints even number between 1 to 50 and display the count.
+
+c_even = 0
+for i in range(50):
+	if i%2==0:
+		print(i)
+		c_even +=1
+print("The number of even numbers = ",c_even)
+
