362accf9599345ee8af13619e9c2c7db50dc4df6
Test2 <Test_email2>
2024-11-19 00:04:05 +0530

    second commit

--- sample_python.py
+++ sample_python.py
@@ -1,10 +1,14 @@
 # This is a sample python file to learn working of git
-# This program just prints even number between 1 to 50 and display the count.
+# This program just prints even number and between 1 to 50 and display the count.
 
 c_even = 0
+c_odd = 0
 for i in range(50):
 	if i%2==0:
 		print(i)
 		c_even +=1
+	else:
+		c_odd +=1
 print("The number of even numbers = ",c_even)
+print("The number of odd numbers = ",c_odd)
 
